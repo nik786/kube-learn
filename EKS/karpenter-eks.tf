ed534331925b42d9453936115e6c1aafcc76977b
nik786 <chakraborty.rock@gmail.com>
2025-07-01 22:53:36 +0530

    Create karpenter-eks.tf

---
+++ EKS/karpenter-eks.tf
@@ -0,0 +1,22 @@
+
+
+- [setup-karpenter-on-your-existing-eks-cluster](https://medium.com/@shadracktanui47/setup-karpenter-on-your-existing-eks-cluster-98bf6e959863)
+- [eksworkshop](https://www.eksworkshop.com/docs/autoscaling/compute/karpenter/configure)
+- [implementing-karpenter-on-eks-and-autoscaling-your-cluster-for-optimal-performance](https://dhruv-mavani.medium.com/implementing-karpenter-on-eks-and-autoscaling-your-cluster-for-optimal-performance-f01a507a8f70)
+
+
+
+## Install Karpenter
+
+helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter \
+  --version "${KARPENTER_VERSION}" \
+  --namespace "karpenter" --create-namespace \
+  --set "settings.clusterName=${EKS_CLUSTER_NAME}" \
+  --set "settings.interruptionQueue=${KARPENTER_SQS_QUEUE}" \
+  --set controller.resources.requests.cpu=1 \
+  --set controller.resources.requests.memory=1Gi \
+  --set controller.resources.limits.cpu=1 \
+  --set controller.resources.limits.memory=1Gi \
+  --set replicas=1 \
+  --wait
+
