1424cbc034a922fae8b78f8d8f83c7e048b927c1
nik786 <chakraborty.rock@gmail.com>
2025-08-31 09:29:15 +0530

    Create provider.tf

---
+++ GCP-TRAINING/use-cases/gke-deployer/gke/provider.tf
@@ -0,0 +1,15 @@
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = file(var.gcp_credentials_file) # best practice: service account JSON
+}
